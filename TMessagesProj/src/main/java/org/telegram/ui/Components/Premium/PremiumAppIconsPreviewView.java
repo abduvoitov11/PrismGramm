9f34ac0c91fbfb1d86c206db567bc3c1943e5a5c
abduvoitov11 <rgobzpe@gmail.com>
2026-08-16 21:35:23 +0500

    feat: unlock all app icons & add 6 custom styled Krypton app icons

--- TMessagesProj/src/main/java/org/telegram/ui/Components/Premium/PremiumAppIconsPreviewView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/Premium/PremiumAppIconsPreviewView.java
@@ -33,18 +33,10 @@ public class PremiumAppIconsPreviewView extends FrameLayout implements PagerHead
         this.resourcesProvider = resourcesProvider;
 
         for (LauncherIconController.LauncherIcon icon : LauncherIconController.LauncherIcon.values()) {
-            if (icon.premium) {
-                icons.add(icon);
-            }
+            icons.add(icon);
             if (icons.size() == 3) {
                 break;
             }
-        }
-
-        if (icons.size() < 3) {
-            FileLog.e(new IllegalArgumentException("There should be at least 3 premium icons!"));
-            isEmpty = true;
-            return;
         }
 
         topIcon = newIconView(context, 0);
