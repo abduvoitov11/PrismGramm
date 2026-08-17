eb163045b122540dc30eb069ac93d77feec8f37f
abduvoitov11 <rgobzpe@gmail.com>
2026-08-17 13:16:37 +0500

    feat: add 25 fantastic and minimalistic iOS 26.5 Liquid Glass PrismGramm app icons

--- TMessagesProj/src/main/java/org/telegram/ui/LauncherIconController.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LauncherIconController.java
@@ -94,7 +94,22 @@ public class LauncherIconController {
         COBALT("CobaltIcon", R.drawable.icon_7_background_sa, R.mipmap.icon_7_foreground_sa, R.string.AppIconCobalt),
         RUBY("RubyIcon", R.drawable.icon_8_background_sa, R.mipmap.icon_8_foreground_sa, R.string.AppIconRuby),
         AURORA("AuroraIcon", R.drawable.icon_9_background_sa, R.mipmap.icon_9_foreground_sa, R.string.AppIconPrismAurora),
-        PURE("PureIcon", R.drawable.icon_10_background_sa, R.mipmap.icon_10_foreground_sa, R.string.AppIconPure);
+        PURE("PureIcon", R.drawable.icon_10_background_sa, R.mipmap.icon_10_foreground_sa, R.string.AppIconPure),
+        MATRIX("MatrixIcon", R.drawable.icon_11_background_sa, R.mipmap.icon_11_foreground_sa, R.string.AppIconPrismMatrix),
+        SUNSET("SunsetIcon", R.drawable.icon_12_background_sa, R.mipmap.icon_12_foreground_sa, R.string.AppIconPrismSunset),
+        COSMOS("CosmosIcon", R.drawable.icon_13_background_sa, R.mipmap.icon_13_foreground_sa, R.string.AppIconPrismCosmos),
+        LAVA("LavaIcon", R.drawable.icon_14_background_sa, R.mipmap.icon_14_foreground_sa, R.string.AppIconPrismLava),
+        CHROME("ChromeIcon", R.drawable.icon_15_background_sa, R.mipmap.icon_15_foreground_sa, R.string.AppIconPrismChrome),
+        SAKURA("SakuraIcon", R.drawable.icon_16_background_sa, R.mipmap.icon_16_foreground_sa, R.string.AppIconPrismSakura),
+        SINGULARITY("SingularityIcon", R.drawable.icon_17_background_sa, R.mipmap.icon_17_foreground_sa, R.string.AppIconPrismSingularity),
+        PLASMA("PlasmaIcon", R.drawable.icon_18_background_sa, R.mipmap.icon_18_foreground_sa, R.string.AppIconPrismPlasma),
+        AMETHYST("AmethystIcon", R.drawable.icon_19_background_sa, R.mipmap.icon_19_foreground_sa, R.string.AppIconPrismAmethyst),
+        CYBER("CyberIcon", R.drawable.icon_20_background_sa, R.mipmap.icon_20_foreground_sa, R.string.AppIconPrismCyber),
+        ABYSS("AbyssIcon", R.drawable.icon_21_background_sa, R.mipmap.icon_21_foreground_sa, R.string.AppIconPrismAbyss),
+        BRONZE("BronzeIcon", R.drawable.icon_22_background_sa, R.mipmap.icon_22_foreground_sa, R.string.AppIconPrismBronze),
+        MONOCHROME("MonochromeIcon", R.drawable.icon_23_background_sa, R.mipmap.icon_23_foreground_sa, R.string.AppIconPrismMonochrome),
+        SPECTRUM("SpectrumIcon", R.drawable.icon_24_background_sa, R.mipmap.icon_24_foreground_sa, R.string.AppIconPrismSpectrum),
+        GLITCH("GlitchIcon", R.drawable.icon_25_background_sa, R.mipmap.icon_25_foreground_sa, R.string.AppIconPrismGlitch);
 
         public final String key;
         public final int background;
